56bf6bb8527bfb10727f7a8bfd3c11505f07c6bc
PRIA IDAMAN <firmanulah@gmail.com>
2016-11-06 23:05:42 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+//KONFIGURASI
+$hostDB		= "localhost";
+$usernameDB = "root";
+$passwordDB = "";
+$namaDB		= "pemesanan";
+
+//KONEKSI KE DATABASE
+$con = mysqli_connect($hostDB,$usernameDB,$passwordDB,$namaDB);
+
+//CEK KONEKSI
+if(mysqli_connect_errno())
+{
+	echo "KONEKSI GAGAL";
+	die;
+}
+
+//AKSES DATABASE
+include("MODELS/database.php");
+
+//MEMANGGIL DATABASE
+$database = new database;
+
+//AKSES CONTROLLERS
+include("CONTROLLERS/controllers.php");
+
+
+?>
